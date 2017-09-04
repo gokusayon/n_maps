129ec914b4b0e1a212832752da7027e97224aec5
Vasu Sheoran <gokusayon@gmail.com>
2017-09-04 16:23:49 +0530

    Fininshed Project for review

---
+++ README.md
@@ -0,0 +1,33 @@
+# n_maps
+FSND: Udacity Project 5 !
+
+## This project includes:
+- main.html   	[home page]
+- app.js  	  	[contains all the default map functions]
+- ko.js 		[contains knockoutjs bindings]
+- styles.css 	[css]
+
+## To Run
+
+
+## Setup
+
+###Google Services set up.
+1. Go to https://console.developers.google.com/project and login.
+2. Create a new project with name 'n_maps'.
+3. Go to Auth -> Credentials and create 'API KEY'
+5  Enable services:
+	- Google Maps JavaScript API
+	- Google Maps Geocoding API
+	- Google Maps Geolocation API
+	- Google Places API Web Service
+	- Google Static Maps API
+	- Google Street View Image API
+
+###Running the application
+
+1. Right click main.html
+2. Open in your favourite browser.
+
+## Author
+**Vasu Sheoran**
--- js/ko.js
+++ js/ko.js
@@ -1,28 +1,14 @@
+// Blank array for all the filtered markers
+var markers_filter = [];
+
 function resizeMap() {
     document.getElementById("map");
     google.maps.event.trigger(map, "resize");
 }
 
-function viewModel() {
-    var self = this;
-    self.shouldShowMessage = ko.observable(true);
-    self.incrementClickCounter = function() {
-        self.shouldShowMessage(!self.shouldShowMessage());
-        
-    }
-    self.optionStatus = ko.pureComputed(function() {
-        return self.shouldShowMessage() ? "options-box" : "options-box-hide";
-    }, viewModel);
-    self.mapStatus = ko.pureComputed(function() {
-        return self.shouldShowMessage() ? "map_wrapper" : "map-only";
-    }, viewModel);
-};
-
-var markers_filter = [];
-
 // This function will loop through the markers array and display them all.
 function showListings(value) {
-    
+
     var bounds = new google.maps.LatLngBounds();
     // Extend the boundaries of the map for each marker and display the marker
     for (var i = 0; i < value.length; i++) {
@@ -32,18 +18,55 @@ function showListings(value) {
     map.fitBounds(bounds);
 }
 
+// This function is used for class bindings
+function viewModel() {
+    var self = this;
+
+    // If the filter window is to be shown
+    self.shouldShowMessage = ko.observable(true);
+    self.showWindow = function() {
+        self.shouldShowMessage(!self.shouldShowMessage());
+        resizeMap();
+    }
+    self.optionStatus = ko.pureComputed(function() {
+        var width = $(window).width();
+        if (width > 529) {
+
+            return self.shouldShowMessage() ? "options-box" : "options-box-hide";
+        } else {
+
+            return self.shouldShowMessage() ? "options-box" : "options-box-hide-with-filter";
+        }
+    }, viewModel);
+    self.mapStatus = ko.pureComputed(function() {
+
+        var width = $(window).width();
+
+        if (width > 529) {
+            return self.shouldShowMessage() ? "map_wrapper" : "map-only";
+        } else {
+
+            return !self.shouldShowMessage() ? "map_wrapper-with-filter" : "map-only";
+        }
+    }, viewModel);
+};
+
+//This function is used for binding user data such as filtered location.
 function mapsModel() {
     var self = this;
     self.filterLocation = ko.observable();
     self.isListVisible = ko.observable(false);
     self.items = ko.observableArray();
-    self.throttledFilterLocation = ko.computed(self.filterLocation).extend({ throttle: 1200 });
+    self.throttledFilterLocation = ko.computed(self.filterLocation)
+        .extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
     self.throttledFilterLocation.subscribe(function(newValue) {
+
+        if (self.filterLocation().length <= 0) {
+            return;
+        }
 
         self.items([]);
         markers_filter = [];
-        // zoomToArea(newValue)
-        // console.log(newValue)
         zoomToArea(newValue).then(function(response) {
             if (response.status) {
 
@@ -56,25 +79,27 @@ function mapsModel() {
                 // mouses over the marker.
                 var highlightedIcon = makeMarkerIcon('FFFF24');
 
-                
+
                 for (var index in list) {
                     var add
                     var item = {
                         location: list[index].geometry.location,
                         address: list[index].formatted_address
                     }
-                    
+
                     var marker = new google.maps.Marker({
                         position: list[index].geometry.location,
-                        title: list[index].formatted_address,
+                        title: list[index].address_components[0].short_name,
                         animation: google.maps.Animation.DROP,
-                        id: index
+                        id: index,
+                        icon:defaultIcon
                     });
+
 
                     markers_filter.push(marker);
                     marker.addListener('click', function() {
                         populateInfoWindow(this, largeInfowindow);
-                    }); 
+                    });
                     // Two event listeners - one for mouseover, one for mouseout,
                     // to change the colors back and forth.
                     marker.addListener('mouseover', function() {
@@ -107,13 +132,28 @@ function mapsModel() {
         // });
         self.items([]);
         self.isListVisible(false);
+        self.filterLocation('');
     }
 
-    self.showListings = function(){
-        showListings(markers)
+    self.showListings = function() {
+        showListings(markers);
+        if (markers_filter.length)
+            showListings(markers_filter);
+
     }
-    self.hideListings = function(){
-        hideListings(markers)
+    self.hideListings = function() {
+        debugger;
+        if (markers.length)
+            hideListings(markers);
+
+        // If filtered locations are present then clear
+        if (markers_filter.length) {
+            hideListings(markers_filter);
+            markers_filter = [];
+            self.items([]);
+            self.isListVisible(false);
+            self.filterLocation('');
+        }
     }
 };
 
@@ -125,3 +165,7 @@ var rootModel = {
 }
 ko.applyBindings(rootModel);
 // ko.applyBindings(new mapsModel());
+
+$(window).resize(function() {
+    resizeMap();
+});
